cd84efb26e37cf407a9c2a096136d966848bba21
SunilGautam <gautam.sunil7725@gmail.com>
2024-03-22 21:07:42 +0545

    Add data transformation function

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import random
 import torch
 import numpy as np
+from torchvision import transforms
 
 
 def reset_random(random_seed=42):
@@ -23,3 +24,32 @@ def reset_random(random_seed=42):
 def get_device():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     return device
+
+
+def get_data_transforms():
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomRotation(20),
+            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+            transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10, resample=False, fillcolor=0),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'validation': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    }
+    return data_transforms
